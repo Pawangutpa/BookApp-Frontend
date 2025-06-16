2537ebf24207a716a8e1c2f970cbc76df7774c24
dsjchd <pawan246g@gmail.com>
2025-06-16 20:35:03 +0530

    url

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,10 +1,10 @@
-const getBaseURL=()=>{
-    return "http://localhost:5000"
-}
-export default getBaseURL;
-
-
 // const getBaseURL=()=>{
-//     return "https://book-app-backend-woad.vercel.app"
+//     return "http://localhost:5000"
 // }
 // export default getBaseURL;
+
+
+const getBaseURL=()=>{
+    return "https://book-app-backend-bay.vercel.app"
+}
+export default getBaseURL;
