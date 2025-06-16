d99b5b110a56a07089fb7a73264dda7b99ae7437
dsjchd <pawan246g@gmail.com>
2025-06-16 18:38:43 +0530

    url

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,4 +1,10 @@
 const getBaseURL=()=>{
-    return "https://book-app-backend-woad.vercel.app"
+    return "https://book-app-frontend-ruby.vercel.app"
 }
 export default getBaseURL;
+
+
+// const getBaseURL=()=>{
+//     return "https://book-app-backend-woad.vercel.app"
+// }
+// export default getBaseURL;
